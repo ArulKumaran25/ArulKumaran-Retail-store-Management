318992053fe98aeed51872e87b1c61fbac60a6d9
Arul Kumaran25 <arulkumaran2519@gmail.com>
2024-12-23 10:47:17 +0530

    validations added for input fields

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,44 +1,60 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-sample',
-  templateUrl:'./sample.component.html',
-  styleUrls: ['./sample.component.css']
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css'],
 })
-
 export class SampleComponent {
-  items =[
-    {id:5001, name:'Soap',price:20},
-    {id:5002,name:'Milk',price:25},
-    {id:5003,name:'Face-Cream',price:30},
-    {id:5004,name:'Diary-Milk',price:40,discount:20},
-    {id:5005,name:'Perfume',price:50,discount:20},
+  items = [
+    { id: 5001, name: 'Curd', price: 20 },
+    { id: 5002, name: 'Milk', price: 25 },
+    { id: 5003, name: 'Butter', price: 30 },
+    { id: 5004, name: 'Ghee', price: 40, discount: 20 },
+    { id: 5005, name: 'Panner', price: 50, discount: 20 },
   ];
-  cart:{id:number;quantity:number;name:string;price:number;total:number }[]=[];
-  selectedItem:any=null;
-  quantity:number=1;
-  totalBill:number = 0;
+  cart: {
+    id: number;
+    quantity: number;
+    name: string;
+    price: number;
+    total: number;
+  }[] = [];
+  selectedItem: any = null;
+  quantity: number = 1;
+  totalBill: number = 0;
 
-  addToCart(){
-    if (this.selectedItem && this.quantity>0) {
-      const discount=this.selectedItem.discount || 0;
-      const priceAfterDiscount=this.selectedItem.price * (1 - discount / 100);
-      const total=priceAfterDiscount * this.quantity;
+  isInvalidQuantity: boolean = false;
+
+  addToCart() {
+    if(this.quantity <= 0) {
+      this.isInvalidQuantity = true; 
+      return;
+    } else {
+      this.isInvalidQuantity = false;
+    }
+
+    if(this.selectedItem && this.quantity > 0) {
+      const discount = this.selectedItem.discount || 0;
+      const priceAfterDiscount =
+        this.selectedItem.price * (1 - discount / 100);
+      const total = priceAfterDiscount * this.quantity;
 
       this.cart.push({
         id: this.selectedItem.id,
-        quantity:this.quantity,
-        name:this.selectedItem.name,
-        price:this.selectedItem.price,
-        total:total,
+        quantity: this.quantity,
+        name: this.selectedItem.name,
+        price: this.selectedItem.price,
+        total: total,
       });
-      this.totalBill+=total;
-      this.selectedItem=null;
-      this.quantity=1;
+      this.totalBill += total;
+      this.selectedItem = null;
+      this.quantity = 1;
     }
   }
-  resetCart(){
-    this.cart=[];
-    this.totalBill=0;
+
+  resetCart() {
+    this.cart = [];
+    this.totalBill = 0;
   }
 }
