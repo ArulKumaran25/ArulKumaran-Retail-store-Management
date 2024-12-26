98d4e698df3cfd6072f18fee7b9c00b7f7a28440
Arul Kumaran25 <arulkumaran2519@gmail.com>
2024-12-26 14:42:19 +0530

    adding features in bill component

--- src/app/bill/bill.component.ts
+++ src/app/bill/bill.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
   styleUrls: ['./bill.component.css']
 })
-export class BillComponent {
+export class BillComponent implements OnInit {
+  cart: any[] = [];
+  totalBill: number = 0;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.cart = JSON.parse(params['cart'] || '[]');
+      this.totalBill = parseFloat(params['totalBill'] || '0');
+    });
+  }
 }
